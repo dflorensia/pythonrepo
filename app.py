25c80980cafe06d89ffa4ea3de1edc15736d7a10
dflorensia <florensia.damayanti@gmail.com>
2017-05-10 15:17:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ if __name__ == '__main__':
     arg_parser = ArgumentParser(
         usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
     )
-    arg_parser.add_argument('-p', '--port', default=8080, help='port')
+    arg_parser.add_argument('-p', '--port', default=8000, help='port')
     options = arg_parser.parse_args()
     print('start server')
     httpd = wsgiref.simple_server.make_server('', options.port, application)
